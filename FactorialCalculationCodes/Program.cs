9dc225ca03121a3d7f93f302bb0df8c6d23a0275
enes2 <enes2@ENES>
2024-01-20 18:33:00 +0300

    Add project files.

---
+++ FactorialCalculationCodes/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace FactorialCalculationCodes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Using the "for" 
+
+            Console.Write("Bir sayı girin: ");
+            int sayi = int.Parse(Console.ReadLine());
+            int faktoriyel = 1;
+            for (int i = sayi; i > 0; i--)
+            {
+                faktoriyel *= i;
+            }
+            Console.Write("Sonuç: {0}", faktoriyel);
+
+            */
+
+            /* Using the "while" */
+
+            Console.Write("Bir sayı girin: ");
+            int sayi = int.Parse(Console.ReadLine());
+            int faktoriyel = 1;
+            while (sayi > 0)
+            {
+                faktoriyel = faktoriyel * sayi;
+
+                sayi--;
+            }
+            Console.Write("Sonuç: {0}", faktoriyel);
+
+            Console.ReadKey();
+        }
+    }
+}
